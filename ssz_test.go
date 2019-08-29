7bb9d8e30923e3030ff872382ee4d0b34f8ebb25
Y.Horie <u5.horie@gmail.com>
2019-08-29 09:00:39 +0900

    Throw Error in SigningRoot if last field is not Signature #76

--- ssz_test.go
+++ ssz_test.go
@@ -22,6 +22,11 @@ type truncateSignatureCase struct {
 	Signature         []byte
 }
 
+type truncateWithoutSignatureCase struct {
+	Slot              uint64
+	PreviousBlockRoot []byte
+}
+
 type simpleNonProtoMessage struct {
 	Foo []byte
 	Bar uint64
@@ -494,6 +499,11 @@ func TestSigningRoot(t *testing.T) {
 			Val1: nil,
 			Err:  errors.New("value cannot be nil"),
 			Val2: nil,
+		},
+		{
+			Val1: &truncateWithoutSignatureCase{Slot: 20},
+			Err:  errors.New("last field has to be signature"),
+			Val2: &truncateWithoutSignatureCase{Slot: 20},
 		},
 	}
 
